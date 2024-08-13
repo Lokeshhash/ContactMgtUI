a6d400310c09a75307c2cbafcc8ebdf1020bb672
Lokeshhash <lokeshrathod360@gmail.com>
2024-08-14 00:04:10 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,110 @@
+
+import { Component, OnInit, inject } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ContactService } from './services/contact.service';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Contact } from './models/contact';
+import { ModeEnum } from './models/mode.enum';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, ReactiveFormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent implements OnInit {
+  private contactService = inject(ContactService);
+  private fb = inject(FormBuilder);
+  form = this.fb.group({
+    id: [0],
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    email: ['', Validators.required],
+  });
+  ModeEnum = ModeEnum;
+  contacts!: Contact[];
+  mode = ModeEnum.NON;
+
+  ngOnInit(): void {
+    this.setContacts();
+  }
+
+  private setContacts() {
+    this.contactService.getAllContacts().subscribe(
+      (data) => {
+        console.log('Data fetched:', data);
+        this.contacts = data;
+      },
+      (error) => {
+        console.error('Error fetching data:', error);
+      }
+    );    
+  }
+
+  addNewContact() {
+    this.mode = ModeEnum.ADD;
+  }
+
+  editContact(user: Contact) {
+    this.mode = ModeEnum.EDIT;
+    this.form.setValue(user);
+  }
+
+  saveContact() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+    const user = this.form.value as Contact;
+
+    if (this.mode === ModeEnum.ADD) {
+      this.contactService.addContact(user).subscribe(
+        (data) => {
+          console.log('Data fetched:', data);                    
+        },
+        (error) => {
+          console.error('Error fetching data:', error);
+        }
+      );
+    } else {
+      //this.contactService.updateUser(user);
+      this.contactService.updateContact(user).subscribe(
+        (data) => {
+          console.log('Data fetched:', data);                    
+        },
+        (error) => {
+          console.error('Error fetching data:', error);
+        }
+      );
+    }
+
+    this.cancel();
+    setTimeout(() => {
+      this.setContacts();      
+    }, 1000);    
+  }
+
+  removeContacts(user: Contact) {
+    //this.contactService.deleteContact(user);
+
+    this.contactService.deleteContact(user).subscribe(
+      (data: any) => {
+        console.log('Data fetched:', data);                    
+      },
+      (error: any) => {
+        console.error('Error fetching data:', error);
+      }
+    );
+
+    this.cancel();
+    setTimeout(() => {
+      this.setContacts();
+    }, 1000);    
+  }
+
+  cancel() {
+    this.form.reset();
+    this.mode = ModeEnum.NON;
+  }
+}
---
+++ src/app/services/contact.service.ts
@@ -0,0 +1,54 @@
+import { Injectable, inject } from '@angular/core';
+import { Contact } from '../models/contact';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ContactService {
+  private http = inject(HttpClient);
+
+  private readonly apiBaseUrl = "https://localhost:7244/api";
+  private readonly contacts: Contact[] = [];
+
+  
+  getAllContacts() {
+    return this.http.get<Contact[]>(`${this.apiBaseUrl}/Contacts`);
+  }
+
+  addContact(user: Contact) {
+    return this.http.post<Contact>(
+      `${this.apiBaseUrl}/Contacts`,
+      user
+    );
+  }
+
+  updateContact(user: Contact) {
+    return this.http.put<Contact>(
+      `${this.apiBaseUrl}/Contacts/${user.id}`,
+      user
+    );
+  }
+
+  deleteContact(user: Contact) {
+    return this.http.delete<Contact>(`${this.apiBaseUrl}/Contacts/${user.id}`);
+  }
+
+  // getAllUsers(): Contact[] {
+  //   return this.contacts;
+  // }
+
+  // addUser(user: Contact) {
+  //   user.id = this.contacts.length + 1;
+  //   this.contacts.push(user);
+  // }
+
+  // updateUser(user: Contact) {
+  //   const index = this.contacts.findIndex((u: Contact) => user.id === u.id);
+  //   this.contacts[index] = user;
+  // }
+
+  // deleteUser(user: Contact) {
+  //   this.contacts.splice(this.contacts.indexOf(user), 1);
+  // }
+}
